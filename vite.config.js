5626c5e176cf4d3f6fcebe383812b5c1c38294eb
hao <971536553@qq.com>
2023-04-03 17:10:19 +0800

    最终版

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,16 @@ import Components from 'unplugin-vue-components/vite';
 import { VantResolver } from 'unplugin-vue-components/resolvers';
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/haoyuan-trip-website/'
+    : '/',
   plugins: [
     vue(),
     Components({
       resolvers: [VantResolver()],
     }),
   ],
-  base:'/haoyuanTrip',
+  base:'/haoyuan-trip-website',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
